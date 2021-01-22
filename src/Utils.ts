82422d60f30be5d3838eae2b55047752eb2fe1f2
Joe <zsklari11@gmail.com>
2021-01-22 13:19:51 +0100

    bound method

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,7 +11,10 @@ class Debouncer {
      * @returns {number} - válaszd ki a kisebbet a num és a max közül 
      * majd válaszd ki a nagyobbat az előbbi érték és a min közül.
      */
-    
+
+    static bound(num: number, min: number, max: number): number {
+        return Math.max(Math.min(num, max), min);
+    }
 
     static debounce<T extends Function>(fun: T, wait: number) {
         let id: any;
