98cc0a5dc6194e445657d4ce2df3fe58f920a7ff
pinprojekt <pin.janos@gmail.com>
2021-01-23 10:28:35 +0100

    piece start but wrong

--- src/Piece.ts
+++ src/Piece.ts
@@ -27,7 +27,11 @@ interface IPiece {
    * 1. A this.type tulajdonságot beállítja a kapott type értékre.
    * 2. Meghívja a this.applyClass metódust a változtatások érvényesítéséhez.
    */
-  setType(type: string): void;
+  
+  setType(type: string): void {
+    this.type = type;
+    this.applyClass();
+  }
 
   /**
    * Osztályokat állít be a this.el HTML elemre.
@@ -35,7 +39,13 @@ interface IPiece {
    * 2. A this.el.classList.add metódussal hozzáad három új osztályt:
    * 'cell', this.type, this.direction
    */
-  applyClass(): void;
+  
+
+  applyClass(): void {
+    this.el.className = '';
+    this.el.classList.add('cell', this.type, this.direction);
+  }
+
 
   /**
    * Megállapítja, hogy ütközött-e a kígyó darabja valamivel.
@@ -45,7 +55,7 @@ interface IPiece {
    * Ha nem, akkor akkor tér vissza true -val, ha a this.x egyenlő a node.x -el 
    * és a this.y egyenlő a node.y -al.
    */
-  isCollidingWith(node: Piece | null): boolean;
+  
 }
 
 /**
@@ -53,6 +63,15 @@ interface IPiece {
  * Pótold a hiányzó tulajdonságokat és metódusokat az interfész alapján.
  */
 export default class Piece implements IPiece {
+
+  next: Piece | null;
+  prev: Piece | null;
+  x: number;
+  y: number;
+  el: HTMLDivElement;
+  direction: string;
+  type: string;
+  garden: HTMLDivElement;
   
   
   
@@ -73,7 +92,7 @@ export default class Piece implements IPiece {
     this.prev = prev;
     // Enable for a neat effect
     // this.el.innerHTML = "&#10096;";
-    this.setType(type);
+    this.setType(this.type);
     this.setPos(this.x, this.y);
     this.garden = (document.getElementById('garden') as HTMLDivElement);
     // this.applyClass();
